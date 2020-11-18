874d13e2e51486f13c5cc6574fa54eee007a1527
mt-satak <sakiware@gmail.com>
2020-11-18 23:07:05 +0900

    restore non core classes

--- web/project/web/index.php
+++ web/project/web/index.php
@@ -1,11 +1,3 @@
 <?php
 
 require '../bootstrap.php';
-require '../MiniBlogApplication.php';
-
-$app = new MiniBlogApplication(false);
-try {
-    $app->run();
-} catch (HttpNotFoundException $e) {
-    echo 'HttpNotFoundException!!!';
-}
